7b2b4823c191874e5d3ef94f01dc616e320f0756
Hannes Achleitner <hannes.software@gmx.at>
2026-02-23 15:00:21 +0100

    Modernize (#285)
    * CI and java 17
    
    Prepare screenshot diff
    
    Remove screenshot Espresso tests preparation
    
    * Prepare kts build
    
    * Android Studio Narwhal settings
    
    * Bump gradle
    
    * Bump NDK build
    
    * Get rid of ButterKnife
    
    * Build with DSL kts
    
    * api36

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.13.2")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    group = "com.github.niqdev"
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.file("build"))
+}
